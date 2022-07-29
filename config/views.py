74c947e534c3ce749f713641428fae28f1a01975
Coding Mantras <ramsinghpunia@gmail.com>
2022-07-30 00:07:13 +0530

    update apps

--- config/views.py
+++ config/views.py
@@ -1,15 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-from singleimages import models
-
 
 class HomeTemplateView(LoginRequiredMixin, TemplateView):
     template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-        images_qs = models.ImageFile.objects.filter(user=user)
-        context["image_qs"] = images_qs
-        return context
